18d216b436224b75ea368a7e60cfdc38f67bf8fd
Vivek Chaurasia <52325013+vivekschaurasia@users.noreply.github.com>
2024-10-15 18:41:28 -0400

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -17,4 +17,49 @@ from keras.utils import to_categorical, plot_model
 from keras.layers import Input, Dense, LSTM, Embedding, Dropout
 
 
-print("HEllo")
+import os
+from tqdm import tqdm
+from keras.preprocessing.image import load_img, img_to_array
+from keras.applications.xception import Xception, preprocess_input
+
+image_model = Xception(weights='imagenet', include_top=False, pooling='avg')
+
+featuresx = {}
+
+directory = '/content/Images'
+
+
+for img_name in tqdm(os.listdir(directory)):
+    # Load the image from file
+    img_path = os.path.join(directory, img_name)
+    image = load_img(img_path, target_size=(299, 299))  # Xception requires (299, 299) input shape
+
+    # Convert image pixels to numpy array
+    image = img_to_array(image)
+
+
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+
+
+    image = preprocess_input(image)
+
+
+    feature = image_model.predict(image, verbose=0)
+
+
+    image_id = img_name.split('.')[0]
+
+    featuresx[image_id] = feature
+
+
+captions_directory = image_directory
+with open(os.path.join(captions_directory, 'captions.txt'), 'r') as f:
+    next(f)
+    captions_doc = f.read()
+
+
+'''
+
+IN PROGRESS
+
+'''
